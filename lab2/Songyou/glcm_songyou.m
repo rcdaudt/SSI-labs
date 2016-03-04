1634cfd609fabaa90b44c51b46cacc80c5083761
pengsongyou <psy920710@gmail.com>
2016-03-04 18:32:17 +0100

    Algorithm implemented in horizontal direction

---
+++ lab2/Songyou/glcm_songyou.m
@@ -0,0 +1,72 @@
+close all
+clear
+clc
+%% Texture features extraction by GLCM
+dir = './P2_seg';
+feli_dir = strcat(dir,'/feli.tif'); 
+hand_dir = strcat(dir,'/hand2.tif'); 
+pingpong_dir = strcat(dir,'/pingpong2.tif'); 
+mosaic_dir = strcat(dir,'/mosaic8.tif'); 
+
+feli_im = imread(feli_dir);
+hand_im = imread(hand_dir);
+mosaic_im = imread(mosaic_dir);
+pingpong_im = imread(pingpong_dir);
+
+im_orig = pingpong_im;
+im = rgb2gray(im_orig);
+
+
+[height,width] = size(im);
+stats_im = zeros(height,width,4);% Images for storing 4 different properties
+
+mask_size = 11;
+hm = floor(mask_size/2); % Half of mask size
+
+for h = hm + 1 : height - hm
+    for w = hm + 1 : width - hm
+        mask_im = im(h-hm:h+hm,w-hm:w+hm);% Acquire 11*11 neighborhood
+        glcm = graycomatrix(mask_im, 'Offset',[0 1],'Symmetric',true);% Compute GLCM matrix of current pixel
+        stats = graycoprops(glcm); % Compute properties 
+        stats_im(h,w,1) = stats.Contrast;
+        stats_im(h,w,2) = stats.Correlation;
+        stats_im(h,w,3) = stats.Energy;
+        stats_im(h,w,4) = stats.Homogeneity;
+    end
+end
+
+% change the NaN in correlation to 1;
+A = stats_im(:,:,2);
+A(isnan(A)) = 1 ;
+stats_im(:,:,2) = A;
+
+% plot the histogram of 4 properties
+figure;
+subplot(421);imshow(stats_im(:,:,1));title('Contrast');
+subplot(423);imshow(stats_im(:,:,2));title('Correlation');
+subplot(425);imshow(stats_im(:,:,3));title('Energy');
+subplot(427);imshow(stats_im(:,:,4));title('Homogeneity');
+
+subplot(422);histogram(stats_im(:,:,1));
+subplot(424);histogram(stats_im(:,:,2));
+subplot(426);histogram(stats_im(:,:,3));
+subplot(428);histogram(stats_im(:,:,4));
+
+%% segmentation
+
+% Create an image containing color and texture features
+im_ct = zeros(height,width,7);% Color and texure mixed images
+im_ct(:,:,1:3) = im_orig;% color part
+im_ct(:,:,4:7) = stats_im;% texture part
+for i = 1 : 7
+    maxval = max(max(im_ct(:,:,i)));
+    minval = min(min(im_ct(:,:,i)));
+    im_ct(:,:,i) = (im_ct(:,:,i) - minval)./maxval;
+end
+% Segment
+im_plot = seg(im_ct(:,:,1:7),[1 1],0.25);
+
+% Plot
+figure;
+imshow(im_plot,[]);
+colormap jet;
---
+++ lab2/Songyou/seg.m
@@ -0,0 +1,60 @@
+function im_label = seg(im,p,delta)
+    [height, width, z] = size(im);
+    if p(1) < 1 || p(1) > height || p(2) < 1 || p(2) > width
+        error('Sorry, the starting point is out of the range!');
+%         exit(0);
+    end
+    im_label = zeros(height, width);
+    nd = [-1 0; 0 1; 1 0; 0 -1]; % neighborhood diretions (In our case is 4 nieghbors)
+    R_label = 1;
+    delta = delta * z;
+    Error = 0;
+
+    % This loop checks if all regions has been segmented
+    im_label(p(1),p(2)) = R_label;
+    while 1 
+        q = p; % The first element in the waiting queue
+        R_mean = im(q(1,1),q(1,2),:);
+        pn = 1; % Initialize the pixel number in the current region
+        
+        % This loop checks if the current region finishes growing alreay
+        while size(q,1) ~= 0 
+            p_cur = q(1,:);  % Always use the first element in the queue as the current point
+            % Visit the neighborhoods of the current point
+            for i = 1 : size(nd,1)
+                p_neighbor = p_cur + nd(i,:);
+
+                % Make sure the neighborhood is valid and not labeled
+                if p_neighbor(1) >= 1 && p_neighbor(2) >=1 && p_neighbor(1) <= height && p_neighbor(2) <= width && im_label(p_neighbor(1),p_neighbor(2)) ==0
+                    for j = 1 : z
+                        Error = Error + abs(im(p_neighbor(1), p_neighbor(2), j) - R_mean(j));
+                    end
+                    if Error <= delta
+                        %give label
+                        im_label(p_neighbor(1),p_neighbor(2)) = R_label; 
+                        %update mean
+                        for j = 1 : z
+                            R_mean(j) = (R_mean(j) * pn + im(p_neighbor(1),p_neighbor(2),j))/(pn+1);
+                        end
+                        %update pixel number in the region
+                        pn = pn + 1;
+                        %insert into the waiting queue
+                        q = [q; p_neighbor];
+                    end
+                    Error = 0;
+                end
+            end
+            q = q(2:end,:);
+        end
+        R_label = R_label + 1; % Update label
+        [h,w] = find(im_label == 0,1,'first');
+        if size(h,1) ~= 0
+            p(1) = h;% Decide the starting point for the next region
+            p(2) = w;
+            im_label(p(1),p(2)) = R_label;
+        else
+            break;
+        end
+    end
+
+end
