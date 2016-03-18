ee540edfa8601488f4a1065db3df00887b789e32
pengsongyou <psy920710@gmail.com>
2016-03-18 21:48:18 +0100

    Final update

--- lab2/Songyou/computeFeatureVector.m
+++ lab2/Songyou/computeFeatureVector.m
@@ -10,21 +10,17 @@ if size(A,3) > 1,
 	A = rgb2gray(A);
 end
 
-% Things I can check
-% 1. symmetric true false
-% 2. 
-
-% direc = [0 12;-12 12; -12 0; -12 -12]; % Different directions for glcm
 % direc = [0 1;-1 1; -1 0; -1 -1;0 2;-2 2; -2 0; -2 -2];% 85 percent
-direc = [0 1;-1 1; -1 0; -1 -1;0 12;-12 12; -12 0; -12 -12;0 23;-23 23; -23 0; -23 -23];% Good one
+% accuracy
+direc = [0 1;-1 1; -1 0; -1 -1;0 12;-12 12; -12 0; -12 -12;0 23;-23 23; -23 0; -23 -23];% 92.5 percent accuracy
 
-v = zeros(1, size(direc,1)*4);
-stats_v = zeros(1,4);
+v = zeros(1, size(direc,1)*4); % feature vectors
+stats_v = zeros(1,4); % tmp statistics vector
 for i = 1 : size(direc,1)
     glcm = graycomatrix(A, 'Offset',direc(i,:),'Symmetric',true);% Compute GLCM matrix of current pixel
     stats = graycoprops(glcm); % Compute properties 
     stats_v(1) = stats.Contrast;
-    if isnan(stats.Correlation) == 1
+    if isnan(stats.Correlation) == 1 % Change all the NaN values to 1
         stats_v(2) = 1;
     else
         stats_v(2) = stats.Correlation;
